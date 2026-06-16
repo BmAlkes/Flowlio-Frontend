dfced365e7a35a5ab08f5fccfb56585a30d524c8
bmalkes <bmalkes@gmail.com>
2026-06-16 22:07:08 +0300

    fix: remove unused Stack import in OverrideLimitsModal

--- src/components/super admin section/subscriptions/OverrideLimitsModal.tsx
+++ src/components/super admin section/subscriptions/OverrideLimitsModal.tsx
@@ -3,7 +3,6 @@ import { GeneralModal } from "@/components/common/generalmodal";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
-import { Stack } from "@/components/ui/stack";
 import { Box } from "@/components/ui/box";
 import {
   DialogHeader,
